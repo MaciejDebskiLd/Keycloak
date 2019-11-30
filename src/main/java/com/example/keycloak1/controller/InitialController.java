34dda70916852b8e9519c415d49f54c2e1f482e5
MaciejDebskiLd <maciej.debski@yahoo.com>
2019-11-30 15:35:38 +0100

    initial

---
+++ src/main/java/com/example/keycloak1/controller/InitialController.java
@@ -0,0 +1,31 @@
+package com.example.keycloak1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitialController {
+
+    @GetMapping(path = "/")
+    public String welcome(){
+        String link = "<a href='http://localhost:8081/users'>Kliknij, żeby się zalogować</a>";
+
+        return "Witaj na stronie logowania systemu PROGRAMATOR " + "<br>" + link;
+    }
+
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String login() {
+        String link = "<a href='http://localhost:8081/logout'>Kliknij, żeby się wylogować</a>";
+
+        return "Witaj wewnątrz systemu PROGRAMATOR "+ "<br>" + link;
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/com/example/keycloak1/Keycloak1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Keycloak1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
